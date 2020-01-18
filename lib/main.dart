18223f45449acc78e2488239bf8ae68ec7569a21
JDNew <chijiaduo@hotmail.com>
2020-01-18 10:30:30 +0800

    improvement ： 修改标题

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: '我的第一个 Flutter Web 项目',
       theme: ThemeData(
         // This is the theme of your application.
         //
